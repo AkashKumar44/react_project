ffe05d3acd86930986fc2419906373b813d48078
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-06-21 14:03:11 +0530

    app.js file add new pages
    update

--- project/src/App.js
+++ project/src/App.js
@@ -3,13 +3,19 @@ import React from "react"
 import './App.css';
 import Banner from './Banner';
 import Navbar from './Navbar';
+import Services from './services/Services';
+import Services from './services/card';
 
   function App(){
 
     return( 
       <>
       <Navbar />
-      <Banner />     
+      <Banner />
+      <Services />
+      <Card />
+
+
          
       </>
       
