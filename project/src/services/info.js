45ce5d84bbe4eb58076bd49822017189b32a5a59
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-06-21 14:04:01 +0530

    info.js file created and update
    update

---
+++ project/src/services/info.js
@@ -0,0 +1,31 @@
+const Menu = [
+
+        {
+            id: 1,
+            image: "../logo192.png",
+            name: "hell",
+            title: "welcome to goa",
+            description: 'A free landing page template built FT Collection website landing page with React JS.'
+        },
+        {
+            id: 2,
+            image: "../logo192.png",
+            name: "hell",
+            title: "welcome to goa",
+            description: 'A free landing page template built FT Collection website landing page with React JS.'
+        },
+        {
+            id: 3,
+            image: "../logo192.png",
+            name: "hell",
+            title: "welcome to goa",
+            description: 'A free landing page template built FT Collection website landing page with React JS.'
+        },
+        {
+            id: 4,
+            image: "../logo192.png",
+            name: "hell",
+            title: "welcome to goa",
+            description: 'A free landing page template built FT Collection website landing page with React JS.'
+        }
+]
