72aced9423fbb27294f815b73ba90f2171e15815
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-06-23 18:50:50 +0530

    file update
    update

--- project/src/services/card.js
+++ project/src/services/card.js
@@ -7,8 +7,6 @@ function GroupExample() {
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-
-          
           <Card.Title>Card title</Card.Title>
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in
