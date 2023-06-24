3f8b1d96c25e934353601b68046d7f6dbf619639
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-06-24 14:19:34 +0530

    add card and file update
    update

--- project/src/services/Services.js
+++ project/src/services/Services.js
@@ -3,32 +3,41 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Img from '../logo192.png'
+// import Img from '../logo192.png'
 
 
-function Services() {
+function Services(val) {  
+  console.log(val);
   return (
     <div className="container text-center py-3"> 
       <h4 className="h1">Our<span> Services</span></h4>
         <p className="h3">What I Do?</p>
         <div className="container py-3"> 
-          <Row xs={1} md={4} className="g-3">
-            {Array.from({ length: 8 }).map((_, idx) => (
-              <Col key={idx}>
-                <Card>
-                  <Card.Img variant="top" src={Img} />
-                  <Card.Body>
-                    <Card.Title>hello</Card.Title>
-                    <Card.Text>
-                      This is a longer card with supporting text below as a natural
-                      lead-in to additional content. This content is a little bit
-                      longer.
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+        {
+          items.map((elem) => {
+            const { id,image,name,title,description } = elem;
+            return(
+                  <Row xs={1} md={4} className="g-3">
+                    {Array.from({ length: 8 }).map((_, idx) => (
+                      <Col>
+                        <Card key={val.id}>
+                          <Card.Img variant="top" src={val.image} />
+                          <Card.Body>
+                            <Card.Title >{val.name}</Card.Title>
+                            <p>{val.title}</p>
+                            <Card.Text>
+                              {val.description}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    ))}
+                  </Row>
+            )
+          })
+
+        }
+          
           
         </div>
     </div>
