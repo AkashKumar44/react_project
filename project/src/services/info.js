7ce880b782b4fe039a1bc140273c270193d627a1
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-06-24 14:19:06 +0530

    services file add content
    update

--- project/src/services/info.js
+++ project/src/services/info.js
@@ -3,8 +3,8 @@ const Menu = [
         {
             id: 1,
             image: "../logo192.png",
-            name: "hell",
-            title: "welcome to goa",
+            name: "hell1",
+            title: "welcome to india",
             description: 'A free landing page template built FT Collection website landing page with React JS.'
         },
         {
@@ -17,15 +17,15 @@ const Menu = [
         {
             id: 3,
             image: "../logo192.png",
-            name: "hell",
-            title: "welcome to goa",
+            name: "hell3",
+            title: "welcome to delhi",
             description: 'A free landing page template built FT Collection website landing page with React JS.'
         },
         {
             id: 4,
             image: "../logo192.png",
-            name: "hell",
-            title: "welcome to goa",
+            name: "hell2",
+            title: "welcome to noida",
             description: 'A free landing page template built FT Collection website landing page with React JS.'
         }
 ]
