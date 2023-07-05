d3cd3688d739cea327400a84336497e074403fd9
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-07-05 10:49:39 +0530

    banner file update text
    update

--- project/src/Banner.js
+++ project/src/Banner.js
@@ -6,16 +6,16 @@ import img from './logo192.png';
 
 const Banner = () => {
 
-        return (
-            <Carousel> 
-              <Carousel.Item>
-                <img className="d-block w-100" src={img} height={200} alt="First slide" />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+    return (
+        <Carousel> 
+          <Carousel.Item>
+            <img className="d-block w-100" src={img} height={200} alt="First slide" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
     );
 }
 
