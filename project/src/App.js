b5b5446ddf0f69422b9df90a6ee71c7a7aa57e4d
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-07-12 16:56:25 +0530

    app file service improve
    update

--- project/src/App.js
+++ project/src/App.js
@@ -3,8 +3,7 @@ import React from "react"
 import './App.css';
 import Banner from './Banner';
 import Navbar from './Navbar';
-
-// import Services from './Services/Services';
+import Services from 'Services/Card.js';
 
   function App(){
 
