5debdc92cf861a9479e7b8a01e7e1196af8e8b00
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-08-21 17:06:02 +0530

    update
    update

---
+++ projet/src/component/Navbar.js
@@ -0,0 +1,19 @@
+// Navbar.js
+import React from 'react';
+// import Navbar from './component/Navbar'
+import Navbar from './Navbar.css'; 
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">Logo</div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
