56c4be55d1b79effc209bf825510b49887b90136
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-08-22 14:20:34 +0530

    new Sdata file add data
    update file

---
+++ projet/src/component/Sdata.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import Card from "./Card.js";
+
+
+    const Sdata = [
+        {
+            imgsrc :"https://img.poki.com/cdn-cgi/image/quality=78,width=600,height=600,fit=cover,f=auto/866858bc3f68d2bb0739b2ef598d9f2d.png",
+            heading : "loop",
+            title : "Welcome to error.Welcome to error.Welcome to error.Welcome to error.",
+            btn : "Read More"
+        },
+    ]
+
+
+
+export default Sdata;
